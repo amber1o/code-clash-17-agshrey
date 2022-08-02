f20c04754323d8425eed4cae3ede3c4ebed797d0
agshrey <68401509+agshrey@users.noreply.github.com>
2022-08-02 10:05:37 -0400

    Add files via upload

---
+++ pascal.py
@@ -0,0 +1,48 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2022
+
+Code Clash #17 - Pascal's Triangle (pascal.py)
+
+
+Author: Paul Thai
+
+Difficulty Level: 6/10
+
+Background: Blaise Pascal is a famous French Mathematician and Philosopher who 
+created Pascal's Triangle. Pascal's Triangle is a triangular array composed of 
+binomial coefficients that arises in probability theory, combinatorics, and algebra. (Wikipedia)
+
+Prompt: Given the number of rows, return a 2D list of Pascal's Triangle
+
+Test Cases:
+Input: rows = 1 ; Output = [[1]]
+Input: rows = 2 ; Output = [[1], [1, 1]]
+Input: rows = 3 ; Output = [[1], [1, 1], [1, 2, 1]]
+
+"""
+
+class Solution:
+    def pascalTri(self,rows):
+        # type row: int
+        # return type: list[list[int]]
+
+        # TODO: Write code below to return a nested list with the solution to the prompt
+        result = [[1]]
+        for line in range(2, rows+2):
+            newline = [1]
+            for i in range(1, line-1):
+                newline.append(result[-1][i-1] + result[-1][i])
+            newline.append(1)
+            result.append(newline)
+        return result
+def main():
+    num = int(input())
+
+    tc1 = Solution()
+    ans = tc1.pascalTri(num)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
